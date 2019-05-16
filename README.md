59d8025020d53e5438782861955e7ebeeefb6847
Lucidastar <lucidahxx@gmail.com>
2019-05-16 15:08:04 +0800

    重新拿起来

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 使用mvp的方式，在详情中用到评论列表  还有单独的评论列表
 ①封装基本的框架 网络 是否加载进度框
 ②当当前页面关闭时停止请求  生命周期
-③一个页面中用到多个presenter，要用到dagger2
+③一个页面中用到多个presenter，要用到dagger2  为了解耦
 ④
 
 要用到的资源：https://github.com/Peakmain/gankzhihu/blob/master/app/src/main/java/com/peakmain/gankzhihu/net/RetrofitManager.java
@@ -13,3 +13,4 @@ https://www.jianshu.com/p/4551529c615e
 https://www.jianshu.com/p/14ff79e9b4c0
 https://github.com/twangithub/TwanBase/tree/master/app/src/main/java/com/twan/base
 https://github.com/RookieExaminer/MvpDemo/blob/master/app/src/main/java/com/example/azheng/rxjavamvpdemo/base/BasePresenter.java
+https://github.com/MirkoWu/BaseLibrary
--- app/src/main/java/com/lucidastar/mysmallcircle/MainActivity.java
+++ app/src/main/java/com/lucidastar/mysmallcircle/MainActivity.java
@@ -2,12 +2,32 @@ package com.lucidastar.mysmallcircle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.internal.DebouncingOnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.tv_test)
+    TextView mTvTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_test)
+    private void test(){
+        mTvTest.setOnClickListener(new DebouncingOnClickListener() {
+            @Override
+            public void doClick(View v) {
+
+            }
+        });
     }
 }
